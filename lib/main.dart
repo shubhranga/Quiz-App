d04a251442fa46d63fb9048f44cc64e883d53595
Shubham Ranga <shubhamranga30@gmail.com>
2025-07-04 00:14:58 +0530

    stub project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: null,
+    );
+  }
+}
+
